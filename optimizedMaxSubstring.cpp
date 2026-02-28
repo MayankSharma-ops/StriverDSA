9c026898aa08fd5c9ea099596b6c5c28522ae0ed
Mayank Sharma <unichain0@gmail.com>
2026-02-28 09:37:27 +0530

    Day-2 Striver DSA

--- optimizedMaxSubstring.cpp
+++ optimizedMaxSubstring.cpp
@@ -1,7 +1,22 @@
 #include <iostream>
 using namespace std;
-int 
+int maxLen(string st){
+    int l=0,r=0,maxLen=0;
+    int n=st.length();
+    int hash[256] = {0};
+    while(r<n){
+        if(hash[st[r]]!=0){
+            if(hash[st[r]]>l){
+                l=hash[st[r]]+1;
+            }
+        }
+        maxLen=max(maxLen,r-l+1);
+        hash[st[r]]=r;
+        r++;
+    }
+    return maxLen;
+}
 int main(){
     string st ="cadbzabcd";
-    return 
+    cout<<maxLen(st);
 }
